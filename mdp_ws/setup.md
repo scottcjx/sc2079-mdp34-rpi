b59e5d57dc3103cbb0ccce2fff45a7323ac4e65c
Scott-CJX-pi1 <scottcjx@gmail.com>
2025-02-21 17:02:42 +0800

    init commit

---
+++ mdp_ws/setup.md
@@ -0,0 +1,4 @@
+
+python3 -m venv venv
+source venv/bin/active
+pip3 install -r requirements.txt
--- scripts/push-all.sh
+++
@@ -1,8 +0,0 @@
-# bash
-
-git fetch
-git pull
-mkdocs gh-deploy
-git add .
-git commit -m "automated commit"
-git push
